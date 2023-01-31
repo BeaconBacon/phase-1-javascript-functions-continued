53a6a80123ba1b5ddaea70ae89fc3c562f7fac27
Christopher <bopherchan@gmail.com>
2023-01-31 14:48:26 -0500

    Comp Functs Continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(whatdo = "go to the office") {
+    return `This Monday, I will ${whatdo}.`
+
+}
+
+function wrapAdjective(sign = "*") {
+    const innerFunc = function (para1 = "special") {
+        return `You are ${sign}${para1}${sign}!`;
+    }
+    return innerFunc;
+
+
+}
